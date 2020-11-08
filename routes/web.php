1ccd6dfe8a254387e9e9f0eebb3a190bf24f0dda
Elia <elia@softwareseni.com>
2020-11-08 21:41:29 +0700

    Hotfix: remove register

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Auth::routes();
+Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [ App\Http\Controllers\Auth\LoginControlle::class, 'logout'])->name('logout');
 
 Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function() {
     
